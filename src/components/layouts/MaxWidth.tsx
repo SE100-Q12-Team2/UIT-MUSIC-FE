7fa1a144a029b342d0fff9fdf6a5b6ee6171317a
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-11-25 17:06:29 +0700

    feat: add landingpage layout components

---
+++ src/components/layouts/MaxWidth.tsx
@@ -0,0 +1,9 @@
+const MaxWidth = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="mx-auto w-full max-w-6xl">
+      {children}
+    </div>
+  )
+}
+
+export default MaxWidth
