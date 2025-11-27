77e7a1d177b58a143e4e6f3fb95c692efc3b420d
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-11-27 20:45:00 +0700

    feat: Implement subscription page with routing and components

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,5 @@
 import LandingPage from "@/features/landing/pages/LandingPage";
+import PremiumSubscriptionsPage from "@/features/subscription/pages/PremiumSubscriptionsPage";
 import { createBrowserRouter } from "react-router";
 
 
@@ -7,5 +8,9 @@ export const router = createBrowserRouter([
     path: '/',
     element: <LandingPage />,
    
+  },
+  {
+    path: '/premium',
+    element: <PremiumSubscriptionsPage />,
   }
 ]);
