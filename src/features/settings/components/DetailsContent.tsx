fb99f4d2966f2952be51fdd23f000eccbc5c9389
23521113@gm.uit.edu.vn <23521113@gm.uit.edu.vn>
2025-12-08 18:04:14 +0700

    feat: add details page components

--- src/features/settings/components/DetailsContent.tsx
+++ src/features/settings/components/DetailsContent.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
+import DetailSection from "./DetailSection";
 
 const DetailsContent: React.FC = () => {
   return (
     <div className="settings-content details-content">
-      <div className="settings-content__placeholder">
-        <h2>Details</h2>
-        <p>Account details content will be displayed here.</p>
-      </div>
+      <DetailSection />
     </div>
   );
 };
