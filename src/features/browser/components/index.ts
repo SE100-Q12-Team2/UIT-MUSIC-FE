e182388100bf66d7da20c40dc7c4bd52c998a0e5
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-12-18 20:17:16 +0700

    feat: components for browser page

---
+++ src/features/browser/components/index.ts
@@ -0,0 +1,5 @@
+export { default as FeaturedCard } from './FeaturedCard';
+export { default as BrowseTrackItem } from './BrowseTrackItem';
+export { default as BrowseTracksSection } from './BrowseTracksSection';
+export type { FeaturedItem } from './FeaturedCard';
+export type { BrowseTrack } from './BrowseTrackItem';
