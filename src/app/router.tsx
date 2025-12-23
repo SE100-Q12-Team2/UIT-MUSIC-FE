9e9779f1625b6edd375cc61cd4954d5e4bcd7c78
23521113@gm.uit.edu.vn <23521113@gm.uit.edu.vn>
2025-12-23 16:55:14 +0700

    feat(layout): integrate MainLayout with app routing

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,20 +1,24 @@
 import App from "@/app/App";
 import LoginPage from "@/features/auth/pages/LoginPage";
-import SignInPage from "@/features/auth/pages/SignInPage";
 import NotFoundPage from "@/features/error/pages/NotFoundPage";
 import LazyLoad from "@/shared/components/common/LazyLoad";
 import MainLayout from "@/shared/layouts/MainLayout";
+import ProtectedRoute from "@/shared/components/ProtectedRoutes";
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router";
 import { PremiumSubscriptionsPage } from "@/features/subscription/pages/PremiumSubscriptionsPage";
 import Home from "@/features/home/pages/HomePage";
 import SettingsPage from "@/features/settings/pages/SettingsPage";
+import RegisterPage from "@/features/auth/pages/RegisterPage";
+import { PlaylistsPage } from "@/features/playlists/pages";
+import { BrowserPage } from "@/features/browser/pages";
+import FavoritePage from "@/features/playlists/pages/FavoritePage";
 
-const LandingPage = lazy(() => import('@/features/landing/pages/LandingPage'));
+const LandingPage = lazy(() => import("@/features/landing/pages/LandingPage"));
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
@@ -22,80 +26,97 @@ export const router = createBrowserRouter([
         children: [
           {
             path: "home",
-            element: <LazyLoad><Home /></LazyLoad>
+            element: (
+              <ProtectedRoute>
+                <LazyLoad>
+                  <Home />
+                </LazyLoad>
+              </ProtectedRoute>
+            ),
           },
           {
             path: "subscriptions",
-            element: <LazyLoad><PremiumSubscriptionsPage /></LazyLoad>
+            element: (
+              <ProtectedRoute>
+                <LazyLoad>
+                  <PremiumSubscriptionsPage />
+                </LazyLoad>
+              </ProtectedRoute>
+            ),
           },
           {
             path: "settings",
-            element: <LazyLoad><SettingsPage /></LazyLoad>
+            element: (
+              <ProtectedRoute>
+                <LazyLoad>
+                  <SettingsPage />
+                </LazyLoad>
+              </ProtectedRoute>
+            ),
           },
-          // {
-          //   path: 'music',
-          //   element: <MusicPage />
-          // },
-          // {
-          //   path: 'playlist',
-          //   element: <PlaylistPage />
-          // },
-          // {
-          //   path: 'playlist/:id',
-          //   element: <PlaylistPage />
-          // },
-          // {
-          //   path: 'artist',
-          //   element: <ArtistPage />
-          // },
-          // {
-          //   path: 'artist/:id',
-          //   element: <ArtistPage />
-          // },
-          // {
-          //   path: 'album',
-          //   element: <AlbumPage />
-          // },
-          // {
-          //   path: 'album/:id',
-          //   element: <AlbumPage />
-          // },
-          // {
-          //   path: 'search',
-          //   element: <SearchPage />
-          // },
-          // {
-          //   path: 'profile',
-          //   element: <ProfilePage />
-          // },
-          // {
-          //   path: 'settings',
-          //   element: <SettingsPage />
-          // }
-        ]
+          {
+            path: "playlists",
+            element: (
+              <LazyLoad>
+                <PlaylistsPage />
+              </LazyLoad>
+            ),
+          },
+
+          {
+            path: "favorite",
+            element: (
+              <LazyLoad>
+                <FavoritePage />
+              </LazyLoad>
+            ),
+          },
+
+          {
+            path: "browser",
+            element: (
+              <LazyLoad>
+                <BrowserPage />
+              </LazyLoad>
+            ),
+          },
+        ],
+      },
+
+      {
+        index: true,
+        element: (
+          <LazyLoad>
+            <LandingPage />
+          </LazyLoad>
+        ),
       },
       {
-          index: true,
-          element: <LazyLoad><LandingPage /></LazyLoad>
+        path: "login",
+        element: (
+          <LazyLoad>
+            <LoginPage />
+          </LazyLoad>
+        ),
       },
       {
-        path: 'login',
-        element: <LazyLoad><LoginPage /></LazyLoad>
+        path: "register",
+        element: (
+          <LazyLoad>
+            <RegisterPage />
+          </LazyLoad>
+        ),
       },
       {
-        path: 'signup',
-        element: <LazyLoad><SignInPage /></LazyLoad>
+        path: "404",
+        element: <NotFoundPage />,
       },
       {
-        path: '404',
-        element: <NotFoundPage />
+        path: "*",
+        element: <Navigate to="/404" replace />,
       },
-      {
-        path: '*',
-        element: <Navigate to="/404" replace />
-      }
-    ]
-  }
+    ],
+  },
 ]);
 
 export default router;
